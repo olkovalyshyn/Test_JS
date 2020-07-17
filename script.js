1ceb78d48670b5193ad77c5dbfab81c87b1bd550
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-07-18 01:14:50 +0300

    Добавляє функції об'єктів

---
+++ script.js
@@ -0,0 +1,21 @@
+// let a = 2;
+// let b = 10;
+
+// c = a + b;
+
+// console.log(c);
+// console.log(typeof a);
+
+let car = {
+  color: "green",
+  maxSpeed: 300,
+  audio: {
+    brand: "Sony",
+    speakers: 10,
+  },
+};
+
+car.color = "yellow";
+
+console.log(typeof car.audio.speakers);
+console.log(car.audio.speakers);
