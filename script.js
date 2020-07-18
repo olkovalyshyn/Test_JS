4ac891611b34c16bce4cbb82e65d02aca8caf1cc
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-07-19 00:52:15 +0300

    Добавляє поняття функцій

--- script.js
+++ script.js
@@ -6,16 +6,70 @@
 // console.log(c);
 // console.log(typeof a);
 
-let car = {
-  color: "green",
-  maxSpeed: 300,
-  audio: {
-    brand: "Sony",
-    speakers: 10,
-  },
+// let car = {
+//   color: "green",
+//   maxSpeed: 300,
+//   audio: {
+//     brand: "Sony",
+//     speakers: 10,
+//   },
+// };
+// console.log(car.color);
+
+// car.color = "yellow";
+// console.log(car.color);
+
+// console.log(typeof car.audio.brand);
+// console.log(car.audio.brand);
+
+// function colorReset() {
+//   car.color = "red";
+//   console.log(car.color);
+// }
+//
+//
+//
+//
+// let car1 = {
+//   color: "red",
+// };
+
+// let car2 = {
+//   color: "green",
+// };
+
+// colorChange(car1);
+// colorChange(car2);
+
+// function colorChange(theCar) {
+//   theCar.color = "yellow";
+// }
+
+// console.log("Car1: " + car1.color);
+// console.log("Car2: " + car2.color);
+
+//
+//
+//
+//
+// ЧОМУСЬ ЦЕ НЕ ПРАЦЮЄ, ХОЧА ВСЕ ПО ВІДЕО
+// let car = {
+//   maxSpeed: 700,
+// };
+
+// let speed = changeSpeed();
+
+// speed();
+
+// function changeSpeed() {
+//   console.log(car.maxSpeed);
+// }
+//
+//
+//
+//
+let anonym = function () {
+  console.log("анoнімна функція");
 };
 
-car.color = "yellow";
-
-console.log(typeof car.audio.speakers);
-console.log(car.audio.speakers);
+anonym();
